7633e10a2fd2c66e1e937c6040fe6f5d6869d2f0
Vanessa Bonito <vanebonito@hotmail.com>
2022-09-20 15:47:15 -0500

    Update weights in animal table

--- queries.sql
+++ queries.sql
@@ -47,4 +47,21 @@ ROLLBACK;
 --Verify if all records in the animals table still exists
 SELECT COUNT(*) FROM animals;
 
+--Transaction 4: update weight
+BEGIN;
+--Delete all animals born after Jan 1st, 2022.
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+--Create a savepoint for the transaction.
+SAVEPOINT SP1;
+--Update all animals' weight to be their weight multiplied by -1.
+UPDATE animals SET weight_kg = weight_kg * -1;
+--Rollback to the savepoint
+ROLLBACK TO SP1;
+--Update all animals' weights that are negative to be their weight multiplied by -1.
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+--Commit transaction
+COMMIT;
+--Verify animals table data after transaction
+SELECT name, weight_kg FROM animals;
+
 
