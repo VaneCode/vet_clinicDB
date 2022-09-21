e0095c95a1d7168e1ecc27b3f782ffe4bcab1168
Vanessa Bonito <vanebonito@hotmail.com>
2022-09-21 10:46:14 -0500

    Popullate table owners

--- data.sql
+++ data.sql
@@ -1,6 +1,7 @@
 /* Populate database with sample data. */
 
---INSERT VALUES IN TABLE ANIMALS
+--TABLE ANIMALS
+--Insert initial data
 INSERT INTO animals (name, date_of_birth, escape_attempts,neutered, weight_kg) VALUES
 ('Agumon','2020-02-03',0,true,10.23),
 ('Gabumon','2018-11-18',2,true,8),
@@ -15,3 +16,14 @@ INSERT INTO animals (name, date_of_birth, escape_attempts,neutered, weight_kg) V
 ('Boarmon','2005-06-07',7,true,20.4),
 ('Blossom','1998-10-13',3,true,17),
 ('Ditto','2022-05-14',4,true,22);
+
+--TABLE OWNERS
+--Popullate table owners
+INSERT INTO owners(full_name, age)
+	VALUES 
+	('Sam Smith',34),
+	('Jennifer Orwell',19),
+	('Bob',45),
+	('Melody Pond',77),
+	('Dean Winchester',14),
+	('Jodie Whittaker',38);
